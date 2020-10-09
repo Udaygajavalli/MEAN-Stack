b31aedb7e891c2abd59599b5aa5489fc5fa100d6
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2020-10-09 12:40:46 +0530

    Fixed Chuck Norris proj

--- chucknorris/script.js
+++ chucknorris/script.js
@@ -7,6 +7,7 @@ function generateRandomJoke() {
     var joke = data.value;
     console.log("JOKE:", joke);
     const q = document.querySelector(".q");
+    q.innerHTML= "";
     const h4 = document.createElement("h4");
     h4.classList.add("card-title");
     h4.classList.add("display-4");
@@ -17,7 +18,22 @@ function generateRandomJoke() {
   .catch();
 }
 
-const btn = document.querySelector(".btn");
+
+const btn = document.querySelector("#generate");
 btn.addEventListener("click", () =>{
   generateRandomJoke();
+})
+
+const clr = document.querySelector("#clear");
+clr.addEventListener("click", () => {
+  const q = document.querySelector(".q");
+  q.innerHTML= "";
+  const h4 = document.createElement("h4");
+  h4.classList.add("card-title");
+  h4.classList.add("display-4");
+  const jokeText = document.createTextNode("Joke will be generated here!");
+  h4.appendChild(jokeText);
+  q.appendChild(h4);
+
+
 })
