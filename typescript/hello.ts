9ed7dbb4c7feea6bb3b95c7688a4cce583ebaf86
Uday Bhaskar Gajavalli <udaygajavalli@gmail.com>
2021-01-12 20:20:44 +0530

    commit typescript

--- typescript/hello.ts
+++ typescript/hello.ts
@@ -1,17 +1,2 @@
-let n: number;
-n= 1+10;
-
-let std:object={};
-
-//array way-1
-let arr:number[] = [10,20,25];
-
-//array way-2
-let arr1:Array<number> = [10,20];
-
-
-let x:any;
-x=10;
-x="abcd";
-x={}
-x=[]
+let sum:number= 10+20;
+console.log("sum is",sum);
